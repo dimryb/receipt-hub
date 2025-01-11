6a01c810bb16f69882b3ca9d5f392c54750c0662
dimryb <dimryb@bk.ru>
2025-01-11 22:45:47 +0700

    Добавил модель данных чека

---
+++ receipt-loader/internal/models/receipt.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+// Receipt представляет данные о чеке
+type Receipt struct {
+	ID           int       `json:"id"`
+	Date         string    `json:"date" binding:"required"` // Формат: YYYY-MM-DD
+	Time         string    `json:"time" binding:"required"` // Формат: HH:mm
+	Amount       float64   `json:"amount" binding:"required"`
+	FiscalNumber string    `json:"fiscal_number" binding:"required"`
+	FiscalDoc    int       `json:"fiscal_document" binding:"required"`
+	FiscalSign   int64     `json:"fiscal_sign" binding:"required"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
+}
