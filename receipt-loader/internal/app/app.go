4118e77c56713aae0af9a151e1ce3c87d0331399
dimryb <dimryb@bk.ru>
2025-01-21 01:04:13 +0700

    Перевел с gin на mux

---
+++ receipt-loader/internal/app/app.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"errors"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"net/http"
+	"os"
+	"receipt-loader/internal/handlers"
+)
+
+type App struct {
+	Config Config
+	Router *mux.Router
+	DB     *gorm.DB
+}
+
+type Config struct {
+	DatabaseUrl string
+}
+
+func (config *Config) Load(filename string) error {
+	err := godotenv.Load(filename)
+	if err != nil {
+		return errors.New("Error loading " + filename)
+	}
+
+	config.DatabaseUrl = os.Getenv("DATABASE_URL")
+	if config.DatabaseUrl == "" {
+		return errors.New("missing DATABASE_URL")
+	}
+
+	return nil
+}
+
+func NewApp() App {
+	return App{}
+}
+
+func (app *App) Setup() error {
+	db, err := gorm.Open(postgres.Open(app.Config.DatabaseUrl), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc(`/receipts/{id:\d+}`, handlers.GetReceiptByID(db)).Methods("GET")
+	r.HandleFunc(`/receipts`, handlers.AddReceipt(db)).Methods("POST")
+
+	app.Router = r
+	app.DB = db
+
+	return nil
+}
+
+func (app *App) Teardown() error {
+	sqlDB, err := app.DB.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Close()
+}
+
+func (app *App) Run() error {
+	return http.ListenAndServe(":8080", app.Router)
+}
--- receipt-loader/internal/handlers/handlers.go
+++ receipt-loader/internal/handlers/handlers.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
 	"errors"
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 	"net/http"
 	"receipt-loader/internal/models"
-	"time"
+	"receipt-loader/internal/rest"
+	"strconv"
 )
 
 // AddReceipt сохраняет данные о чеке
@@ -21,97 +23,86 @@ import (
 // @Failure 409 {object} models.ErrorResponse "Чек уже существует"
 // @Failure 500 {object} models.ErrorResponse "Ошибка сервера"
 // @Router /receipts [post]
-func AddReceipt(c *gin.Context, db *gorm.DB) {
-	var receipt models.Receipt
+func AddReceipt(db *gorm.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var receipt models.Receipt
 
-	// Привязка JSON к модели
-	if err := c.ShouldBindJSON(&receipt); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   "Invalid input",
-			"details": err.Error(),
+		err := json.NewDecoder(r.Body).Decode(&receipt)
+		if err != nil {
+			rest.WriteError(w, http.StatusBadRequest, err)
+			return
+		}
+
+		// Проверка на существование дубликата по ключевым полям
+		existingReceipt := models.Receipt{}
+		err = db.Where("fiscal_number = ? AND fiscal_document = ? AND fiscal_sign = ?",
+			receipt.FiscalNumber,
+			receipt.FiscalDocument,
+			receipt.FiscalSign,
+		).Take(&existingReceipt).Error
+
+		if err == nil {
+			// Дубликат найден
+			rest.WriteJSON(w, http.StatusConflict, rest.Response{
+				Ok:     false,
+				Error:  "Receipt already exists",
+				Result: existingReceipt.Id,
+			})
+			return
+		} else if !errors.Is(err, gorm.ErrRecordNotFound) {
+			// Ошибка при проверке
+			rest.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		err = db.Create(&receipt).Error
+		if err != nil {
+			rest.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		rest.WriteJSON(w, http.StatusCreated, rest.Response{
+			Ok:     true,
+			Result: receipt.Id,
 		})
-		return
-	}
-
-	// Проверка на существование дубликата по ключевым полям
-	existingReceipt := models.Receipt{}
-	err := db.Where("fiscal_number = ? AND fiscal_document = ? AND fiscal_sign = ?",
-		receipt.FiscalNumber,
-		receipt.FiscalDocument,
-		receipt.FiscalSign,
-	).First(&existingReceipt).Error
-	if err == nil {
-		// Дубликат найден
-		c.JSON(http.StatusConflict, gin.H{
-			"error": "Receipt already exists",
-			"id":    existingReceipt.ID,
-		})
-		return
-	} else if err != gorm.ErrRecordNotFound {
-		// Ошибка при проверке
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to verify receipt uniqueness",
-		})
-		return
-	}
-
-	// Установка времени создания
-	receipt.CreatedAt = time.Now()
-
-	// Сохранение данных в базу
-	if err := db.Create(&receipt).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   "Failed to save receipt",
-			"details": err.Error(),
-		})
-		return
-	}
-
-	// Успешный ответ
-	c.JSON(http.StatusCreated, gin.H{
-		"message": "Receipt saved successfully",
-		"id":      receipt.ID,
 	})
 }
 
-// GetReceiptByID возвращает чек по его ID
-// @Summary Get receipt by ID
-// @Description Retrieve a receipt by its unique ID.
+// GetReceiptByID возвращает чек по его Id
+// @Summary Get receipt by Id
+// @Description Retrieve a receipt by its unique Id.
 // @Tags Receipts
 // @Accept json
 // @Produce json
-// @Param id path string true "Receipt ID"
+// @Param id path string true "Receipt Id"
 // @Success 200 {object} models.Receipt
 // @Failure 404 {object} models.ErrorResponse "Чек не найден"
 // @Failure 500 {object} models.ErrorResponse "Ошибка сервера"
 // @Router /receipts/{id} [get]
-func GetReceiptByID(c *gin.Context, db *gorm.DB) {
-	id := c.Param("id")
-	var receipt models.Receipt
-
-	// Поиск чека по ID
-	if err := db.First(&receipt, id).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "Receipt not found",
-			})
-		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "Failed to retrieve receipt",
-			})
+func GetReceiptByID(db *gorm.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		idStr := vars["id"]
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			rest.WriteError(w, http.StatusBadRequest, err)
+			return
 		}
-		return
-	}
 
-	c.JSON(http.StatusOK, receipt)
-}
+		var receipt models.Receipt
+		err = db.Take(&receipt, id).Error
+		if err != nil {
+			if err == gorm.ErrRecordNotFound {
+				rest.WriteError(w, http.StatusNotFound, errors.New("not found"))
+				return
+			}
+			rest.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
 
-// SetupRoutes инициализирует маршруты API
-func SetupRoutes(router *gin.Engine, db *gorm.DB) {
-	router.POST("/receipts", func(c *gin.Context) {
-		AddReceipt(c, db)
-	})
-	router.GET("/receipts/:id", func(c *gin.Context) {
-		GetReceiptByID(c, db)
+		rest.WriteJSON(w, http.StatusOK, rest.Response{
+			Ok:     true,
+			Result: receipt,
+		})
 	})
 }
